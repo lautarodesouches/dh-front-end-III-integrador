8094e81d8e4a21070c6acd0055287dffeef9a7e1
Lautaro Desouches <lautarodesouches@outlook.com>
2023-06-28 12:14:49 -0300

    Detalle de odontologo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 1,
     'react/jsx-no-target-blank': 0,
-    'react/prop-types': 0
+    'react/prop-types': 0,
+    'react-hooks/exhaustive-deps': 0
   }
 }
--- src/pages/Detalle/index.jsx
+++ src/pages/Detalle/index.jsx
@@ -1,7 +1,43 @@
+import { useParams } from 'react-router-dom'
 import styles from './styles.module.css'
+import { useEffect, useState } from 'react'
 
 export default function Detalle() {
-    return(
-        <h1 className={styles.container}>Detalle</h1>
+    const { id } = useParams()
+
+    const [odontologo, setOdontologo] = useState({})
+
+    const getOdontologo = async () => {
+        try {
+            const response = await fetch(
+                `https://jsonplaceholder.typicode.com/users/${id}`
+            )
+            if (response.status !== 200) throw new Error('Not found')
+            const data = await response.json()
+            setOdontologo(data)
+        } catch (error) {
+            setOdontologo(null)
+        }
+    }
+
+    useEffect(() => {
+        getOdontologo()
+    }, [])
+
+    return (
+        <>
+            {odontologo ? (
+                <article className={styles.container}>
+                    <h2 className={styles.title}>{odontologo.name}</h2>
+                    <p className={styles.text}>Email: {odontologo.email}</p>
+                    <p className={styles.text}>Teléfono: {odontologo.phone}</p>
+                    <p className={styles.text}>Sitio web: {odontologo.website}</p>
+                </article>
+            ) : (
+                <h2 className={styles.title}>
+                    Odontologo con id {id} no encontrado
+                </h2>
+            )}
+        </>
     )
 }
