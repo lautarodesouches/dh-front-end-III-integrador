33de0b01a93de871cdaca0eb47f6625bfd670ba8
Lautaro Desouches <lautarodesouches@outlook.com>
2023-06-28 13:27:32 -0300

    Contacto

--- src/pages/Contacto/index.jsx
+++ src/pages/Contacto/index.jsx
@@ -1,5 +1,73 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
 
 export default function Contacto() {
-    return <h1 className={styles.container}>Contacto</h1>
+    const [name, setName] = useState({ value: '', error: false })
+    const [email, setEmail] = useState({ value: '', error: false })
+    const [formOk, setFormOk] = useState(false)
+
+    const handleNameChange = (e) => {
+        const value = e.target.value.trim()
+        let error = false
+        if (value.length < 6) error = 'Tiene que tener mas de 6 caracteres'
+        setName({ value: value, error })
+    }
+
+    const handleEmailChange = (e) => {
+        const value = e.target.value
+        let error = false
+        if (!value.includes('@')) error = 'Tiene que tener un @'
+        setEmail({ value: value, error })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setFormOk(true)
+    }
+
+    return (
+        <>
+            {formOk ? (
+                <h2 className={styles.title}>
+                    Gracias {name.value}, te contactaremos a por {email.value}
+                </h2>
+            ) : (
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <h1 className={styles.title}>¡Contáctanos!</h1>
+                    <div className={styles.div}>
+                        <label className={styles.label} htmlFor="name">
+                            Nombre
+                        </label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            name="name"
+                            placeholder="Juan"
+                            id="name"
+                            value={name.value}
+                            onChange={(e) => handleNameChange(e)}
+                        />
+                        {name.error && <p className={styles.error}>{name.error}</p>}
+                    </div>
+                    <div className={styles.div}>
+                        <label className={styles.label} htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            name="email"
+                            placeholder="juan@email.com"
+                            id="email"
+                            value={email.value}
+                            onChange={(e) => handleEmailChange(e)}
+                        />
+                        {email.error && <p className={styles.error}>{email.error}</p>}
+                    </div>
+                    <button className={styles.button}>Enviar</button>
+                </form>
+            )}
+        </>
+    )
 }
